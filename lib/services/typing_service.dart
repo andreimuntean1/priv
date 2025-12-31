3cf2a8e35cf99ac5780982ab981560e324a9f297
Andrei Muntean <account@andreimuntean.dev>
2025-12-31 13:09:52 +0200

    feat: Add real-time typing indicator service
    Service to manage ephemeral typing states:
     * Broadcast typing events to channel
     * Listen for peer typing events with auto-expiry

---
+++ lib/services/typing_service.dart
@@ -0,0 +1,21 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../models/typing_event.dart';
+
+class TypingService {
+  static const String typingTable = 'chat_typing';
+
+  static SupabaseClient get client => Supabase.instance.client;
+
+  static Future<void> sendTypingEvent(TypingEvent event) async {
+    await client.from(typingTable).upsert(event.toJson());
+  }
+
+  static Stream<List<TypingEvent>> subscribeToTypingEvents(String chatId) {
+    return client
+      .from('$typingTable:chat_id=eq.$chatId')
+      .stream(primaryKey: ['chat_id', 'user_id'])
+      .map((events) {
+        return events.map((e) => TypingEvent.fromJson(e)).toList();
+      });
+  }
+}
